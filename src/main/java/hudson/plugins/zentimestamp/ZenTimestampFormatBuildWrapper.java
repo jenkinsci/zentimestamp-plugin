c46e54b186ff3f361a20aee527bbd060b1303b26
gbois <gbois@dev.java.net>
2010-01-27 23:07:04 +0000

    Updated to new Hudson API

--- src/main/java/hudson/plugins/zentimestamp/ZenTimestampFormatBuildWrapper.java
+++ src/main/java/hudson/plugins/zentimestamp/ZenTimestampFormatBuildWrapper.java
@@ -6,59 +6,43 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
 import hudson.tasks.BuildWrapper;
-
 import hudson.tasks.BuildWrapperDescriptor;
+import hudson.util.FormValidation;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Map;
 
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.StaplerRequest;
 
-
-/**
- * ZenTimestampFormatBuildWrapper {@link Builder}.
- *
- * <p>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link ZenTimestampFormatBuildWrapper} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
- *
- * <p>
- * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)} method
- * will be invoked. 
- *
- * @author Gregory BOISSINOT - Zenika
- */
 public class ZenTimestampFormatBuildWrapper extends BuildWrapper {
 
-	private String pattern;
-	
-	ZenTimestampFormatBuildWrapper(String pattern){
-		this.pattern = pattern;
-	}
-	
-	
-	public String getPattern() {
-		return pattern;
-	}
+    private String pattern;
+
+    @DataBoundConstructor
+    public ZenTimestampFormatBuildWrapper(String pattern) {
+        this.pattern = pattern;
+    }
 
 
-	@Override
-	public hudson.tasks.BuildWrapper.Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws java.io.IOException, java.lang.InterruptedException{	
+    public String getPattern() {
+        return pattern;
+    }
 
-		final PrintStream logger = listener.getLogger();				
-		Calendar buildTimestamp = build.getTimestamp();
-		logger.println("Formating the BUILD_ID variable with'"+  pattern + "' pattern.");
+
+    @Override
+    public hudson.tasks.BuildWrapper.Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws java.io.IOException, java.lang.InterruptedException {
+
+        final PrintStream logger = listener.getLogger();
+        Calendar buildTimestamp = build.getTimestamp();
+        logger.println("Formating the BUILD_ID variable with'" + pattern + "' pattern.");
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-        final String newBUILDIDStr = sdf.format(buildTimestamp.getTime());      
+        final String newBUILDIDStr = sdf.format(buildTimestamp.getTime());
 
         return new Environment() {
-            
+
             @Override
             public void buildEnvVars(Map<String, String> env) {
                 env.put("BUILD_ID", newBUILDIDStr);
@@ -70,8 +54,8 @@ public class ZenTimestampFormatBuildWrapper extends BuildWrapper {
     /**
      * Descriptor for {@link ZenTimestampFormatBuildWrapper}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
+     * <p/>
+     * <p/>
      * See <tt>views/hudson/plugins/zentimestamp/ZenTimestampFormatBuildWrapper/*.jelly</tt>
      * for the actual HTML fragment for the configuration screen.
      */
@@ -86,7 +70,7 @@ public class ZenTimestampFormatBuildWrapper extends BuildWrapper {
          * This human readable name is used in the configuration screen.
          */
         public String getDisplayName() {
-            return "Change BUILD_ID format";
+            return Messages.ZenTimestampFormatBuildWrapper_displayName();
         }
 
         @Override
@@ -95,12 +79,28 @@ public class ZenTimestampFormatBuildWrapper extends BuildWrapper {
         }
 
         /**
-         * Creates a new instance of {@link ZenTimestampFormatBuildWrapper} from a submitted form.
+         * Checks if the provided pattern is valuid
          */
-        @Override
-        public ZenTimestampFormatBuildWrapper newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return new ZenTimestampFormatBuildWrapper(req.getParameter("zentimestamp.pattern"));
+        public FormValidation doCheckPattern(@QueryParameter String value) {
+
+            if (value == null || value.trim().length() == 0) {
+                return FormValidation.error(Messages.ZenTimestampFormatBuildWrapper_emptyPattern());
+            }
+
+            try {
+                new SimpleDateFormat(value);
+            }
+            catch (NullPointerException npe) {
+                return FormValidation.error(Messages.ZenTimestampFormatBuildWrapper_invalidInput(npe.getMessage()));
+            }
+            catch (IllegalArgumentException iae) {
+                return FormValidation.error(Messages.ZenTimestampFormatBuildWrapper_invalidInput(iae.getMessage()));
+            }
+
+            return FormValidation.ok();
         }
-    }	
+
+
+    }
 
 }
