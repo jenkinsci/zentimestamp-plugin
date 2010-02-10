4c7e41f9486f6392145c6b23b0410e281ff705ac
gbois <gbois@dev.java.net>
2010-02-10 19:44:38 +0000

    Fixed import

--- src/ittest/java/hudson/plugins/zentimestamp/ZenTimestampFormatBuildWrapperTest.java
+++ src/ittest/java/hudson/plugins/zentimestamp/ZenTimestampFormatBuildWrapperTest.java
@@ -1,17 +1,12 @@
 package hudson.plugins.zentimestamp;
 
-import hudson.EnvVars;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.tasks.Shell;
-import hudson.util.LogTaskListener;
 import org.jvnet.hudson.test.HudsonTestCase;
 
 import java.text.SimpleDateFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Date;
 
 
 public class ZenTimestampFormatBuildWrapperTest extends HudsonTestCase {
