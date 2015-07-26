eca0b9bdc96e70c6a1d423886b6070eea033150c
gboissinot <gregory.boissinot@gmail.com>
2015-07-27 00:26:40 +0200

    Remove serialUID, unused

--- src/main/java/hudson/plugins/zentimestamp/ZenTimestampLogger.java
+++ src/main/java/hudson/plugins/zentimestamp/ZenTimestampLogger.java
@@ -7,8 +7,6 @@ import java.io.Serializable;
 
 public class ZenTimestampLogger implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     public static void log(BuildListener listener, final String message) {
         listener.getLogger().println("[zentimestamp] " + message);
     }
